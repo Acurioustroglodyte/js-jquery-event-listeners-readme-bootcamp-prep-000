349ba360d92e0c99e118c448d8c9094b16e659bc
Trevor Jameson <tjameson15@gmail.com>
2018-06-03 14:09:31 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -25,7 +25,7 @@ function submitIt() {
 submitIt();
 
 function pressIt() {
-  $('form').on('keyPress', function(e) {
+  $('input').on('keyPress', function(e) {
     if (e.key == 'g') {
       alert("You have pressed G.")
     }
